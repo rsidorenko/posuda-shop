4e237bfa36fc33740302e5d4390553694a028e42
rsidorenko <123245853+rsidorenko@users.noreply.github.com>
2025-05-23 10:59:29 +0300

    Delete posuda-shop-backend directory

--- posuda-shop-backend/middleware/adminMiddleware.js
+++
@@ -1,9 +0,0 @@
-const isAdmin = (req, res, next) => {
-    if (req.user && req.user.isAdmin) {
-        next(); // всё ок, пропускаем дальше
-    } else {
-        res.status(403).json({ message: "Доступ запрещён: только для администраторов" });
-    }
-};
-
-module.exports = isAdmin;
